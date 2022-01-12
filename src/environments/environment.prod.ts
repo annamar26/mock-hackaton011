9f5be734a06cd0f3bb3b2d70d160321c2b8f8cb3
annamar26 <ag.lo.am26@gmail.com>
2022-01-12 14:41:51 -0600

    add firebase config to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBTznl6WGuXaCUcmyD3p0MdlhRY8W3jlBA",
+    authDomain: "total-look-c675e.firebaseapp.com",
+    projectId: "total-look-c675e",
+    storageBucket: "total-look-c675e.appspot.com",
+    messagingSenderId: "356615159504",
+    appId: "1:356615159504:web:ac11ddc8950ae55a7368d5",
+    measurementId: "G-T04SY1VB8M"
+}
 };
